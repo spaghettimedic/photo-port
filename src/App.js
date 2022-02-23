a80c7321492e475c72753a53f4fd143a2e679292
spaghettimedic <dsteiner247@gmail.com>
2022-02-23 15:16:40 -0500

    control state of ContactForm as a controlled component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -34,11 +35,12 @@ function App() {
       >
       </Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory} ></Gallery>
         <About></About>
       </main>
     </div>
   );
-}
+};
 
 export default App;
