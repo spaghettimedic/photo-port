151d389b68587988122c0fa468eb308e407abe37
spaghettimedic <dsteiner247@gmail.com>
2022-02-23 16:41:26 -0500

    validated form data; conditionally render: error message for form validation, contact form, nav styling

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
   ]);
   
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -32,12 +33,19 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       >
       </Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory} ></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
